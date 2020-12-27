091e9b8ea09953037398cd56345c16246e016353
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-27 14:38:13 +0900

    check TextView behavior

--- app/src/main/java/com/example/myfirstandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidkotlin/MainActivity.kt
@@ -3,29 +3,35 @@ package com.example.myfirstandroidkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 // まずはonCreateというメソッドがAndroidによって呼び出されます
 // =>エントリーポイントという。
+// class hoge : fuga() でextendsの意味
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val hello: String = "Hello World"
-        val int_value: Int = 12345 //型宣言しなくても可
-        val long_value: Long = 12345L //Long型のリテラル
-        val double_value: Double = 12.345
-        val float_value: Float = 12.345F
-        val boolean_value: Boolean = true
+//        val hello: String = "Hello World"
+        val intValue: Int = 12345 //型宣言しなくても可
+        val longValue: Long = 12345L //Long型のリテラル
+        val doubleValue: Double = 12.345
+        val floatValue: Float = 12.345F
+        val booleanValue: Boolean = true
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("MainActivity", hello);
-        Log.d("MainActivity", "" + int_value);
-        Log.d("MainActivity", "" + long_value);
-        Log.d("MainActivity", "" + double_value);
-        Log.d("MainActivity", "" + float_value);
-        Log.d("MainActivity", "" + boolean_value);
+        //R.id.text_view_helloはレイアウトエディタで自動で振り分けられる定数(Int)
+        val hello: TextView = findViewById(R.id.text_view_hello)
+        Log.d("MainActivity", hello.text.toString())
+
+//        Log.d("MainActivity", hello);
+        Log.d("MainActivity", "" + intValue);
+        Log.d("MainActivity", "" + longValue);
+        Log.d("MainActivity", "" + doubleValue);
+        Log.d("MainActivity", "" + floatValue);
+        Log.d("MainActivity", "" + booleanValue);
         Log.d("MainActivity", "" + this.addition(5, 19));
-        Log.d("MainActivity", "" + hello.replace("World", "Android"));
+//        Log.d("MainActivity", "" + hello.replace("World", "Android"));
 
         /* log output
         2020-12-26 23:27:03.392 13106-13106/com.example.myfirstandroidkotlin D/MainActivity: Hello World
@@ -36,7 +42,8 @@ class MainActivity : AppCompatActivity() {
         2020-12-26 23:27:03.393 13106-13106/com.example.myfirstandroidkotlin D/MainActivity: true
         */
     }
-    private fun addition (a: Int, b: Int) : Int {
+
+    private fun addition(a: Int, b: Int): Int {
         return a + b
     }
 }
